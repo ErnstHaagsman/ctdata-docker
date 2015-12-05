ae9ab00bf67ded4b98fbc25d55993205aaf83b91
Ernst Haagsman <ernsthaagsman@gmail.com>
2015-12-05 00:56:37 -0800

    Add webapp

--- datanode/start_after_wait.sh
+++ datanode/start_after_wait.sh
@@ -3,4 +3,4 @@
 sleep 10
 
 mysql --host=mysql --user=ctdata --password=use_this_password ctdata < schema.sql
-java -jar DataNode.jar -dbconfig db-config.json -queueconfig queue-config.json
+java -jar DataNode.jar -config config.json
