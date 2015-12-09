2d8f82201dbf86f70ac144aa33b4a79e5da72204
Ernst Haagsman <ernsthaagsman@gmail.com>
2015-12-08 18:23:56 -0800

    Two raspnodes

--- getjars.sh
+++ getjars.sh
@@ -4,7 +4,9 @@ wget -N -O jars.zip https://s3-us-west-1.amazonaws.com/ctdata-builds/build/$1.zi
 unzip -o jars.zip
 
 mv jars/CtdataSensorGateway-*.jar ./sensorgateway/SensorGateway.jar
-mv jars/CtdataRaspNodeSim-*.jar ./raspnodesim/RaspNodeSim.jar
 mv jars/CtdataOrchestrationServer-*.jar ./orchestrator/Orchestrator.jar
 mv jars/CtdataDatanodeServer-*.jar ./datanode/DataNode.jar
 mv jars/gs-spring-boot-*.jar ./webapp/WebApp.jar
+
+cp jars/CtdataRaspNodeSim-*.jar ./raspnode-1/RaspNodeSim.jar
+mv jars/CtdataRaspNodeSim-*.jar ./raspnode-2/RaspNodeSim.jar
